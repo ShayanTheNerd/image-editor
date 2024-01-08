7898c5677a0642dc231d0a16485a3adfe8ba52d5
Shayan Zamani <sh.zamani384@gmail.com>
2024-01-08 20:18:14 +0330

    ♻️ refactor: update to Tailwind v3.4.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./index.html', './assets/js/app.js', './assets/js/modules/*.mjs'],
 	darkMode: ['class', '[data-theme="dark"]'],
 	theme: {
 		extend: {
-			screens: {
-				support: { raw: '(hover: hover)' }, // check if the device supports hover
-			},
 			colors: {
 				platinum: '#e2e1e0',
 				royalBlue: '#2C52ED',
