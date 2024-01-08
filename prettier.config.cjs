f3c70ac907551b69b1be701fa287bdefbc325f7b
Shayan Zamani <sh.zamani384@gmail.com>
2024-01-08 20:28:02 +0330

    🧹 chore: update `.gitignore` and Prettier configs

---
+++ prettier.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+	semi: true,
+	useTabs: true,
+	printWidth: 120,
+	singleQuote: true,
+	trailingComma: 'all',
+	insertPragma: false,
+	arrowParens: 'avoid',
+	requirePragma: false,
+	bracketSpacing: true,
+	proseWrap: 'preserve',
+	bracketSameLine: true,
+	quoteProps: 'as-needed',
+	htmlWhitespaceSensitivity: 'css',
+	embeddedLanguageFormatting: 'auto',
+	plugins: ['prettier-plugin-tailwindcss'],
+};
